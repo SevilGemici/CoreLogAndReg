d24d7e59db736a467f099c210d17dd370680f68e
SevilGemici <svlgemici22@gmail.com>
2020-12-06 22:24:48 +0300

    Initial commit

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CoreLogAndReg.Areas.Identity.Data;
+using CoreLogAndReg.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CoreLogAndReg.Areas.Identity.IdentityHostingStartup))]
+namespace CoreLogAndReg.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CoreLogAndRegContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("CoreLogAndRegContextConnection")));
+
+                services.AddDefaultIdentity<CoreLogAndRegUser>(options => { options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                })
+                    .AddEntityFrameworkStores<CoreLogAndRegContext>();
+            });
+        }
+    }
+}
